16a422b0c77b7821ebff529fb1e588f0a85b11e8
Hindjanati <100366241+Hindjanati@users.noreply.github.com>
2022-11-01 09:44:57 +0100

    algorithme about xo logic

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.css']
 })
-export class SquareComponent implements OnInit {
+export class SquareComponent {
+  @Input() value!: 'X' | 'O' ;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
